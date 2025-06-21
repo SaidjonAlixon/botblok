0f973842918ea6b83446dd8e89719f9de900a3ba
SaidjonAlixon <saidjonalixon@gmail.com>
2025-06-21 14:09:07 +0500

    fix: Pass env var directly to pip install

--- build.sh
+++ build.sh
@@ -5,8 +5,5 @@ set -o errexit
 # Install build dependencies
 apt-get update && apt-get install -y libpq-dev pkg-config
 
-# Set environment variable to avoid Rust build for asyncpg
-export ASYNCPG_NO_RUST=1
-
-# Install Python dependencies
-pip install -r requirements.txt 
+# Install Python dependencies, forcing non-rust build for asyncpg
+ASYNCPG_NO_RUST=1 pip install -r requirements.txt 
