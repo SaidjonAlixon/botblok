5d54043dd8467a9f50bf5fd5ab156414fb4ef6fb
SaidjonAlixon <saidjonalixon@gmail.com>
2025-06-21 14:06:33 +0500

    fix: Set ASYNCPG_NO_RUST in build script

--- build.sh
+++ build.sh
@@ -5,5 +5,8 @@ set -o errexit
 # Install build dependencies
 apt-get update && apt-get install -y libpq-dev pkg-config
 
+# Set environment variable to avoid Rust build for asyncpg
+export ASYNCPG_NO_RUST=1
+
 # Install Python dependencies
 pip install -r requirements.txt 
